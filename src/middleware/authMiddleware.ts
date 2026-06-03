6244bae594e614b8a9f38a343834909b7c50f995
sanjayraj18 <22bsm052@iiitdmj.ac.in>
2026-06-03 16:32:54 +0530

    feat: implement access token verification middleware

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,3 +1,47 @@
-const verifyAccessToken = () => {};
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import config from "../config";
+import { TokenService } from "../service/TokenService";
+
+interface AuthRequest extends Request {
+  userId?: string;
+  sessionId?: string;
+}
+
+const verifyAccessToken = async (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) {
+      return res.status(409).json({
+        message: "Token not found",
+      });
+    }
+    const decoded = jwt.verify(token, config.JWT_SECRET) as any;
+    const sessionId = decoded.sessionId;
+
+    const sessionFromRedis = await TokenService.getSession(sessionId);
+    if (!sessionFromRedis) {
+      return res.status(409).json({
+        message: "Session not found",
+      });
+    }
+    if (sessionFromRedis.revoked) {
+      return res.status(409).json({
+        message: "Session revoked",
+      });
+    }
+
+    req.userId = decoded?.id;
+    req.sessionId = decoded?.sessionId;
+  } catch (err) {
+    res.status(401).json({
+      message: "Token verification failed from middleware",
+    });
+  }
+};
 
 export default verifyAccessToken;
